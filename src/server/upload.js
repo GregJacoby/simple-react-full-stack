9c95bc319f753ba956cece95c89688c45e55864e
Greg <lightening48@gmail.com>
2019-04-19 10:25:06 -0400

    formidable upload logic

---
+++ src/server/upload.js
@@ -0,0 +1,17 @@
+const IncomingForm = require("formidable").IncomingForm;
+
+module.exports = function upload(req, res) {
+     var form = new IncomingForm();
+
+    form.parse(req);
+
+    form.on('fileBegin', function (name, file){
+        file.path = __dirname + '/uploads/' + file.name;
+    });
+
+    form.on('file', function (name, file){
+        console.log('Uploaded ' + file.name);
+    });
+
+    res.sendFile(__dirname + '/index.html');
+};
